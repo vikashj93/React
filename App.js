0abf39dcccca41c6bbc7080218a4040017cb8661
vikashj93 <vikash.oswal1993@gmail.com>
2024-12-24 15:45:00 +0530

    dummy commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@
         ]),
         React.createElement("div", {id:"child2"}, [
             React.createElement("h1", {}, "I'm an h1 tag"),
-            React.createElement("h2", {}, "I'm an h2 tag"),
+            React.createElement("h2", {}, "I'm  an h2 tag"),
         ]),
     ]);
 
