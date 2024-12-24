76ac914e4c0c54950d40aeb212c8d8c49907886e
vikashj93 <vikash.oswal1993@gmail.com>
2024-12-24 15:56:30 +0530

    hotfixes

--- src/api.js
+++ src/api.js
@@ -5,24 +5,24 @@ export const fetchTransactions = () => {
           {
             customerId: 1,
             transactions: [
-              { date: "2024-01-05", amount: 120 },
-              { date: "2024-01-10", amount: 80 },
-              { date: "2024-01-15", amount: 55 },
+              { date: "2024-01-05", amount: 160 },
+              { date: "2024-01-10", amount: 70 },
+              { date: "2024-01-15", amount: 85 },
             ]
           },
           {
             customerId: 2,
             transactions: [
-              { date: "2024-02-03", amount: 150 },
-              { date: "2024-02-15", amount: 95 },
-              { date: "2024-02-20", amount: 60 },
+              { date: "2024-02-03", amount: 190 },
+              { date: "2024-02-15", amount: 75 },
+              { date: "2024-02-20", amount: 95 },
             ]
           },
           {
             customerId: 3,
             transactions: [
-              { date: "2024-03-12", amount: 220 },
-              { date: "2024-03-18", amount: 110 },
+              { date: "2024-03-12", amount: 240 },
+              { date: "2024-03-18", amount: 170 },
             ]
           }
         ]);
